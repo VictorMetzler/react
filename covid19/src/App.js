afbb142aaff33fc5a645e7f42170a9f6c53a88b6
VictorMetzler <victor.metzler@ciaathletica.com.br>
2020-09-14 16:39:34 -0300

    Covid - Final

--- covid19/src/App.js
+++ covid19/src/App.js
@@ -3,10 +3,12 @@ import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 import { CssBaseline } from "@material-ui/core";
+import coronaImage from './images/image.png';
 
 export default class App extends React.Component {
   state = {
     data: {},
+    country: ''
   };
 
   async componentDidMount() {
@@ -14,15 +16,25 @@ export default class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
+
+  handleCountryChange = async (country) => {
+    // fetch the data
+    // set the state
+    const fetchedData = await fetchData(country);
+    this.setState({data: fetchedData, country: country});
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
+    
     return (
       <React.Fragment>
         <CssBaseline />
         <div className={styles.container}>
+          <img classname={styles.image} src={coronaImage} alt="COVID-19" />
           <Cards data={data} />
-          <CountryPicker />
-          <Chart />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country}/>
         </div>
       </React.Fragment>
     );
