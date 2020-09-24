c2b22f9def4a076a780eb40f5dc8314822d8c0a4
VictorMetzler <victor.metzler@ciaathletica.com.br>
2020-09-24 16:50:03 -0300

    Seasons - setStates

--- udemy/seasons/src/index.js
+++ udemy/seasons/src/index.js
@@ -1,12 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return "Hi there!";
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state, when initialize
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState !!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // React says, we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>this.state.errorMessage</div>
+    }
+
+    if (!this.state.errorMessage)
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
