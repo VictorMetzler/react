f80c4914fe7b060b0648145182364fd820d8395e
VictorMetzler <victor.metzler@ciaathletica.com.br>
2020-09-25 14:28:38 -0300

    Season - Last commit

--- udemy/seasons/src/index.js
+++ udemy/seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -12,17 +13,20 @@ class App extends React.Component {
     );
   }
 
-  // React says, we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>this.state.errorMessage</div>;
+      return <div>{this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading...</div>;
+    return <Spinner message="Please, accept location request." />;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
