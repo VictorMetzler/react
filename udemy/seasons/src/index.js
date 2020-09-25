e89b99cb609d79f34f0e7552e014ec98da541416
VictorMetzler <victor.metzler@ciaathletica.com.br>
2020-09-25 11:34:00 -0300

    Seasons - Icons

--- udemy/seasons/src/index.js
+++ udemy/seasons/src/index.js
@@ -1,31 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    // THIS IS THE ONLY TIME we do direct assignment
-    // to this.state, when initialize
-    this.state = { lat: null, errorMessage: "" };
+  state = { lat: null, errorMessage: "" };
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setState !!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   // React says, we have to define render!!
   render() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>this.state.errorMessage</div>
+      return <div>this.state.errorMessage</div>;
     }
 
-    if (!this.state.errorMessage)
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
