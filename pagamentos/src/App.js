8ff49290aa7963e14e40f1389e15b92e8c6ecd71
VictorMetzler <victor.metzler@ciaathletica.com.br>
2022-06-29 14:10:43 -0300

    sync

---
+++ pagamentos/src/App.js
@@ -0,0 +1,23 @@
+const Header = () => {
+  return <div>Here goes the header</div>;
+};
+
+const Main = () => {
+  return <div>Here goes the Main content</div>;
+};
+
+const Footer = () => {
+  return <div>Here goes the footer</div>;
+};
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
