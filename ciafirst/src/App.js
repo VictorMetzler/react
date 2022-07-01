226eb6da29e462f2c064bf4699fec731346ce781
VictorMetzler <victor.metzler@ciaathletica.com.br>
2022-07-01 15:07:30 -0300

    sync

--- ciafirst/src/App.js
+++ ciafirst/src/App.js
@@ -4,9 +4,9 @@ import DP_Unidades from "./components/DP_Unidades/DP_Unidades";
 export default class extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <React.Str>
         <DP_Unidades />
-      </React.Fragment>
+      </React.Str>
     );
   }
 }
