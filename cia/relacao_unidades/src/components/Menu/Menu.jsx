75249fd4fa92ad3cb0e499783075db2c3e965c0e
Victor Albert Metzler <60222475+VictorMetzler@users.noreply.github.com>
2023-07-05 20:47:59 -0300

    050723_cia_ru_fim

--- cia/relacao_unidades/src/components/Menu/Menu.jsx
+++ cia/relacao_unidades/src/components/Menu/Menu.jsx
@@ -1,14 +1,46 @@
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import "./Menu.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-export const Menu = () => {
+export const Menu = (props) => {
   return (
-    <Container fluid>
-      <Row>
-        <Col>1 of 1</Col>
-      </Row>
-    </Container>
+    <Navbar
+      style={{
+        backgroundColor: props.bg_color ? props.bg_color : "transparent",
+      }}
+    >
+      <Container>
+        <Navbar.Brand>
+          {props.img_path ? (
+            <img
+              src={props.img_path}
+              style={{ maxWidth: "70%" }}
+              alt="Cia Athletica"
+            />
+          ) : (
+            ""
+          )}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
