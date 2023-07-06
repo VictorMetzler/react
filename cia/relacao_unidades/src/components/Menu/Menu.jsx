47415844a69e849622ca086e094dbdf7999ced61
VictorMetzler <victor.metzler@ciaathletica.com.br>
2023-07-06 11:52:43 -0300

    060723_cia_ru_menu

--- cia/relacao_unidades/src/components/Menu/Menu.jsx
+++ cia/relacao_unidades/src/components/Menu/Menu.jsx
@@ -13,28 +13,53 @@ export const Menu = (props) => {
       }}
     >
       <Container fixed="top" className="p-0">
-        <Navbar expand="xxl" variant="dark" className="p-0 text-center">
-          <Container className="col-12 col-md-8 justify-content-center justify-content-md-start p-0">
-            <NavbarBrand className="ms-2 ms-md-0 col-7">
-              {props.img_path ? (
+        {props.img_path && !props.titulo ? (
+          <Navbar
+            expand="xxl"
+            variant="dark"
+            className="col-12 p-0 text-center"
+          >
+            <Container className="col-12 p-0">
+              <NavbarBrand className="col-12">
                 <Image
                   src={props.img_path}
                   fluid
                   alt="Cia Athletica"
-                  style={{ maxWidth: "65%" }}
+                  width="116px"
                 />
-              ) : (
-                ""
-              )}
-            </NavbarBrand>
-            <div className="d-none d-md-block d-lg-block d-xl-block me-0 me-md-3 text-white">
-              |
-            </div>
-            <NavbarBrand className="mb-2 mb-md-0 ms-2 ms-md-0 ms-md-3 text-normal">
-              {props.titulo}
-            </NavbarBrand>
-          </Container>
-        </Navbar>
+              </NavbarBrand>
+            </Container>
+          </Navbar>
+        ) : !props.img_path && props.titulo ? (
+          <Navbar
+            expand="xxl"
+            variant="dark"
+            className="col-12 p-0 text-center"
+          >
+            <Container className="justify-content-center p-0">
+              <NavbarBrand className="text-normal">{props.titulo}</NavbarBrand>
+            </Container>
+          </Navbar>
+        ) : (
+          <Navbar expand="xxl" variant="dark" className="p-0 text-center">
+            <Container className="col-12 col-md-8 justify-content-center justify-content-md-start p-0">
+              <NavbarBrand className="ms-2 ms-md-0 col-7 p-2">
+                <Image
+                  src={props.img_path}
+                  fluid
+                  alt="Cia Athletica"
+                  style={{ maxWidth: "70%" }}
+                />
+              </NavbarBrand>
+              <div className="d-none d-md-block d-lg-block d-xl-block me-0 me-md-3 text-white">
+                |
+              </div>
+              <NavbarBrand className="mb-2 mb-md-0 ms-2 ms-md-0 ms-md-3 text-normal">
+                {props.titulo}
+              </NavbarBrand>
+            </Container>
+          </Navbar>
+        )}
       </Container>
     </Navbar>
   );
