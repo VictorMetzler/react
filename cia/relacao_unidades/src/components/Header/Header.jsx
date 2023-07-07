e362fb0f127ecf72b48b3074f06b91ab1673a02a
VictorMetzler <victor.metzler@ciaathletica.com.br>
2023-07-07 14:22:54 -0300

    070723_cia_ru_header_termino

--- cia/relacao_unidades/src/components/Header/Header.jsx
+++ cia/relacao_unidades/src/components/Header/Header.jsx
@@ -32,32 +32,28 @@ export const Header = (props) => {
           </Navbar>
         ) : (
           <Navbar
-            variant="dark"
-            className="col p-0 text-center text-md-start justify-content-center justify-content-md-start"
-            style={{ border: "1px dotted blue" }}
+            expand="xxl"
+            data-bs-theme="dark"
+            className="col-12 p-1 m-0 justify-content-center justify-content-md-start"
           >
-            <Container
-              className="col p-0 text-center text-md-start justify-content-center justify-content-md-start"
-              style={{ border: "1px dotted green" }}
-            >
-              <NavbarBrand
-                className="ms-md-0 col-12 col-md-2 p-1"
-                style={{ border: "1px dotted yellow" }}
-              >
-                <Image
-                  src={props.img_path}
-                  fluid
-                  alt="Cia Athletica"
-                  style={{ maxWidth: "70%" }}
-                />
-              </NavbarBrand>
-              <div className="d-none d-md-block d-lg-block d-xl-block mb-md-1 me-0 me-md-3 text-white">
-                |
-              </div>
-              <NavbarBrand className="mb-1 ms-md-3 mt-1 mt-md-0">
-                {props.titulo}
-              </NavbarBrand>
-            </Container>
+            <NavbarBrand className="p-0 m-0 col-12 col-md-auto me-md-4 mt-1 mt-md-0 text-center text-md-end">
+              <Image
+                className="p-0 m-0"
+                src={props.img_path}
+                alt="CiaAthletica"
+                style={{
+                  maxWidth: "35%",
+                  minWidth: "120px",
+                  boxSizing: "content-box",
+                }}
+              />
+            </NavbarBrand>
+            <div className="d-none d-md-block d-lg-block d-xl-block ms-md-1 me-0 me-md-3 text-white">
+              |
+            </div>
+            <NavbarBrand className="p-0 m-0 mb-2 mb-md-0 ms-0 ms-md-3 mt-3 mt-md-0">
+              {props.titulo}
+            </NavbarBrand>
           </Navbar>
         )}
       </Container>
